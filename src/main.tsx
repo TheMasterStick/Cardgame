76797f70bba8bf5453944348bae7b406aba37db3
TheMasterStick <87497026+TheMasterStick@users.noreply.github.com>
2026-08-24 22:01:53 +0300

    Resolve multi-format card art in React app

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { CARD_DEFINITIONS } from "./data/cards";
+import { resolveCardDefinitionArtPaths } from "./card-rendering/artAsset";
 import { AuthProvider } from "./lib/AuthProvider";
 import "./ui/styles.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </StrictMode>,
-);
+async function bootstrap() {
+  await resolveCardDefinitionArtPaths(CARD_DEFINITIONS);
+
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </StrictMode>,
+  );
+}
+
+void bootstrap();
